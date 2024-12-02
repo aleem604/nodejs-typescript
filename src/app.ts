149d58ad45913ad9343068c546fa4effeb41fd34
aleem <aleem604@gmail.com>
2024-12-03 03:40:11 +0500

    type fixes

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,13 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.use((req, res, next) => {
+  console.log(req.url, "from middleware");
+  next();
+});
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World from TS!6!");
 });
 
 app.listen(port, () => {
